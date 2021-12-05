c34ac30cee158070027ef3005ba8481813580157
NouraAshraff <62406338+NouraAshraff@users.noreply.github.com>
2021-12-05 22:01:06 +0200

    Update Router.java

--- .vscode/Router.java
+++ .vscode/Router.java
@@ -1,6 +1,15 @@
-
+import java.util.ArrayList;
 
 public class Router {
     
-    
+    ArrayList<Integer> connections ;
+
+
+    public void occupy(){
+
+    }
+    public void release(){
+        
+    }
+
 }

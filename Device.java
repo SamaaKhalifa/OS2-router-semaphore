1c41c0f9dc93095506f62c2aa652fa7b73cb3659
bassant samer <61835135+bassantsamerr@users.noreply.github.com>
2021-12-06 22:17:03 +0200

    update

--- Device.java
+++ Device.java
@@ -2,11 +2,14 @@ package OS2-router-semaphore;
 class Device implements Runnable{
     private String name;
     private String type;
-
-    public Device(String type,String name) {
+    private Router router;
+    public Device(String type,String name,Router router) {
         this.name = name;
         this.type = type;
+        this.router = router;
     }
+
+    public void setRouter(Router router) {this.router = router;}
 
     public void setName(String name) {
         this.name = name;
@@ -24,7 +27,11 @@ class Device implements Runnable{
         return type;
     }
 
-    public void connect(){}
-    public void performOnlineActivity(){}
-    public void logout(){}
+    public Router getRouter() {return router;}
+
+    public void connect(){router.occupy();}
+    public void performOnlineActivity(){sleep(5000);}
+    public void logout(){router.release();}
+    @Override
+
 }
